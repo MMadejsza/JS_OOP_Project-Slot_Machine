56d46874132b9c0208318f15d8adb0b08478c7fd
MMadejsza <maciej.madejsza@gmail.com>
2022-11-25 18:10:30 +0000

    this.changeWallet if condition

--- Wallet.js
+++ Wallet.js
@@ -3,8 +3,22 @@ class Wallet {
 		let _money = money;
 		this.getWalletValue = () => _money;
 		this.checkIfCanPlay = (value) => {
+			//-check if still has enough money
 			if (_money >= value) return true;
 			return false;
+		};
+		this.changeWallet = (value, typeOfOperation = '+') => {
+			//- adding/subtracting money in wallet
+			if (typeof value == 'number' && !isNaN(value)) {
+				//- if given is number and not "not a number fault"
+				if (typeOfOperation === '+') {
+					return (_money += value);
+				} else if (typeOfOperation === '-') {
+					return (_money -= value);
+				} else {
+					throw new Error('invalid operation type');
+				}
+			}
 		};
 	}
 }
