dda50b607210b529a404d32d5adb1e9bc9ff6a4b
MMadejsza <maciej.madejsza@gmail.com>
2022-11-25 18:21:08 +0000

    this.gameResults test examples

--- Statistics.js
+++ Statistics.js
@@ -1,3 +1,10 @@
 class Statistics {
-	constructor() {}
+	constructor() {
+		this.gameResults = [
+			{win: true, bid: 2},
+			{win: false, bid: -20},
+			{win: true, bid: 15},
+		];
+	}
 }
+const stats = new Statistics();
