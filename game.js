7c357a29ca7a29291d85c7f000dd2582c3e4b91d
MMadejsza <maciej.madejsza@gmail.com>
2022-11-25 19:32:04 +0000

    DOM fetched, frame created

--- game.js
+++ game.js
@@ -1,5 +1,21 @@
 class Game {
-	constructor() {}
+	constructor(startWallet) {
+		//- initiating previous classes
+		this.stats = new Statistics();
+		this.wallet = new Wallet(startWallet);
+		document.querySelector('#start').addEventListener('click', this.startGame);
+		this.spanWallet = document.querySelector('.panel span.wallet');
+		this.tiles = document.querySelectorAll('div.tile');
+		this.inputBid = document.querySelector('#bid');
+		this.spanResult = document.querySelector('.score div.result');
+		this.spanResult = document.querySelector('.score span.number');
+		this.spanResult = document.querySelector('.score div.win');
+		this.spanResult = document.querySelector('.score div.loss');
+		this.render();
+	}
 	startGame() {}
-	render() {}
+	render() {
+		console.log('lets start');
+	}
 }
+const game = new Game(200);
