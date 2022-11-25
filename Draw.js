753fd209c667fc052dece8207e02e4ca2c2da8e6
MMadejsza <maciej.madejsza@gmail.com>
2022-11-25 19:04:27 +0000

    class comments

--- Draw.js
+++ Draw.js
@@ -1,14 +1,14 @@
 class Draw {
 	constructor() {
-		this.options = ['red', 'green', 'blue'];
-		let _result = this.drawResult();
+		this.options = ['red', 'green', 'blue']; //- what are the options to draw in the game
+		let _result = this.drawResult(); // - when initiating class it's assign straight away
 		this.getDrawResult = () => _result;
 	}
 	drawResult() {
-		let colors = [];
+		let colors = []; //- we want result in form of array
 		for (let i = 0; i < this.options.length; i++) {
 			const index = Math.floor(Math.random() * this.options.length);
-			const color = this.options[index];
+			const color = this.options[index]; //- [options.length] times draw tile
 			console.log(color);
 			colors.push(color);
 		}
