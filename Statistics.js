889e633baabf0acf1eb86b4013f985326b4fc135
MMadejsza <maciej.madejsza@gmail.com>
2022-11-25 18:45:27 +0000

    showGameStats() finished

--- Statistics.js
+++ Statistics.js
@@ -7,9 +7,16 @@ class Statistics {
 		];
 	}
 	addGameResultToStats(win, bid) {
+		//? may be { win, bid} and automatically will create following:
 		let gameResult = {win: win, bid: bid};
 		console.log(gameResult);
 		this.gameResults.push(gameResult);
+	}
+	showGameStats() {
+		let gamesNumber = this.gameResults.length;
+		let winsNumber = this.gameResults.filter((result) => result.win == true).length;
+		let lossesNumber = this.gameResults.filter((result) => result.win == false).length;
+		return [gamesNumber, winsNumber, lossesNumber];
 	}
 }
 const stats = new Statistics();
