d57c8f5a000ab21c4e3a7c52754ce1fd5d560af4
MMadejsza <maciej.madejsza@gmail.com>
2022-11-25 19:21:06 +0000

    Result finished

--- Result.js
+++ Result.js
@@ -1,4 +1,16 @@
+//- we are writing down rules of the game. We don't need constructor because this class is just to return result of the game and secondly what user won.
 class Result {
-	constructor() {}
+	static moneyWonInGame(result, bid) {
+		if (result) return 3 * bid;
+		else return 0;
+	}
+	static checkIfWon(drawResult) {
+		if (
+			(draw[0] === draw[1] && draw[1] === draw[2]) ||
+			(draw[0] !== draw[1] && draw[1] !== draw[2] && draw[0] !== draw[2])
+		) {
+			return true;
+		} else return false;
+	}
 }
-const result = new Result();
+Result.moneyWonInGame(true, 12);
