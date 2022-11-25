391696094622a8baba7591dc826dbf5b9d073bc9
MMadejsza <maciej.madejsza@gmail.com>
2022-11-25 19:01:29 +0000

    class finished

--- Draw.js
+++ Draw.js
@@ -1,3 +1,19 @@
 class Draw {
-	constructor() {}
+	constructor() {
+		this.options = ['red', 'green', 'blue'];
+		let _result = this.drawResult();
+		this.getDrawResult = () => _result;
+	}
+	drawResult() {
+		let colors = [];
+		for (let i = 0; i < this.options.length; i++) {
+			const index = Math.floor(Math.random() * this.options.length);
+			const color = this.options[index];
+			console.log(color);
+			colors.push(color);
+		}
+		return colors;
+	}
 }
+
+const draw = new Draw();
